6387c37b9a63fe223c17e08cb5d6eac2e64e6cf3
pansettykarthik <karthikpansetty@gmail.com>
2021-05-11 00:36:11 -0400

    Updated training code

--- train.py
+++ train.py
@@ -70,6 +70,7 @@ def train_model(train_loader, model, hparams, criterion, optimizer):
 def evaluate_model(val_loader, model, hparams, criterion):
     predictions = []
     actuals = []
+    val_loss = 0
 
     # distances = []
 
@@ -115,6 +116,7 @@ def evaluate_model(val_loader, model, hparams, criterion):
         del input_len_ratio
 
         torch.cuda.empty_cache()
+        val_loss += loss.item()
 
     p = generate_all_outputs(predictions)
     a = generate_all_outputs(actuals)
@@ -124,8 +126,8 @@ def evaluate_model(val_loader, model, hparams, criterion):
         print("ERROR")
 
     dist = calc_levenshtein_dist(a, p)
-
-    return dist, loss.item()
+    val_loss /= len(val_loader)
+    return dist, val_loss
 
 
 def run_model(train_loader, val_loader, hparams, criterion, optimizer, scheduler):
@@ -158,14 +160,13 @@ def run_model(train_loader, val_loader, hparams, criterion, optimizer, scheduler
 
 
 if __name__ == '__main__':
-
     LETTER_LIST = ['<', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
                    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', "'", '.', '_', '+', ' ', '>']
 
     letter2index, index2letter = create_dictionaries(LETTER_LIST)
 
     BASE_PATH = ""
-    DATA_PATH = os.path.join(BASE_PATH, "data/")
+    DATA_PATH = os.path.join(BASE_PATH, "data-simple/")
     MODEL_PATH = os.path.join(BASE_PATH, "models/")
     PREDICTION_PATH = os.path.join(BASE_PATH, "predictions/")
 
@@ -178,11 +179,10 @@ if __name__ == '__main__':
 
     # test_filename = os.path.join(DATA_PATH, "test.npy")
 
-
     device = get_device()
     num_workers = get_num_workers()
-
     hparams = get_hyperparameters()
+    hparams["num_workers"] = num_workers
 
     model = Wav2Letter(hparams["out_vocab"])
     model.to(device)
@@ -192,5 +192,6 @@ if __name__ == '__main__':
     optimizer = torch.optim.AdamW(model.parameters(), lr=hparams["learning_rate"], weight_decay=hparams["weight_decay"])
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=5)
 
-    train_loader, val_loader = get_dataloaders(train_filename, train_transcripts_filename, dev_filename, dev_transcripts_filename, letter2index, hparams)
+    train_loader, val_loader = get_dataloaders(train_filename, train_transcripts_filename, dev_filename,
+                                               dev_transcripts_filename, letter2index, hparams)
     run_model(train_loader, val_loader, hparams, criterion, optimizer, scheduler)
