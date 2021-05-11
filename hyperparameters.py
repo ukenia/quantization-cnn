866793818f393fe0c72b882c425401716b981b0e
pansettykarthik <karthikpansetty@gmail.com>
2021-05-11 00:37:34 -0400

    Updated hyperparamaters

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,12 +1,9 @@
 hparams = {
     "epochs": 250,
-    "learning_rate": 1e-6,
+    "learning_rate": 1e-4,
     "num_features": 40,
     "out_vocab": 35, # Including Blanks
     "weight_decay": 5e-5,
-
-    "max_lr": 5e-3,
-    "gamma": 0.95,
     "batch_size": 128,
 }
 
